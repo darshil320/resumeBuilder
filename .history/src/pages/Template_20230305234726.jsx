d57df4ef2108a5d69bf4b3d4ef4b7f28d81ac569
Darshil lashkari <lashkaridarshil@gmail.com>
2023-03-06 20:01:08 +0530

    version-2

---
+++ .history/src/pages/Template_20230305234726.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Template extends Component {
+  render() {
+    return (
+      <div>Template</div>
+    )
+  }
+}
+
+export default Template
---
+++ .history/src/pages/Template_20230305234739.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Template extends Component {
+  render() {
+    return (
+      <div>This is Template</div>
+    )
+  }
+}
+
+export default Template
